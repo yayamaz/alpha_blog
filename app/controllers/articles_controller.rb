54e140d07288f33a0810928d3f2eb940f5435439
Yanis Saheb <yanis.saheb@gmail.com>
2018-05-18 17:59:40 +0000

    fix edit user bug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -52,9 +52,13 @@ class ArticlesController < ApplicationController
   end
   
   private
+  
+  
   def set_article
     @article = Article.find(params[:id])
   end 
+  
+  
   def article_params
     params.require(:article).permit(:title, :description)
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
   def new
     @user= User.new
   end
-  
+  def show
+    @user=User.find(params[:id])
+  end
   def create
     @user = User.new(user_params)
     if @user.save
@@ -13,12 +15,32 @@ class UsersController < ApplicationController
     end
     
   end
+ 
   
-  private
   def user_params
     
     params.require(:user).permit(:username, :email, :password)
   end
+   
+
+  def edit
+   @user = User.find(params[:id])
+    
+  end 
+  
+  def update
+  @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      
+      render 'edit'
+    end
+    
+  
+  end
+  
   
   
   
